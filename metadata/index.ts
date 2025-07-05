9bc1561a16132a9355502928ab60c4975c4770bc
yux829 <yux829@sina.com>
2025-07-05 17:28:29 +0800

    01 new

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = 'ITCoder'
+export const siteShortName = 'ITCoder'
+export const siteDescription = 'IT笔记记录'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
